f8e35c5d226b71f1a93ffdb70993d44c82062cca
Ryo TAKAISHI <ryo.takaishi.0@gmail.com>
2017-11-14 21:53:48 +0900

    default gatewayはここで変更

--- provision.sh
+++ provision.sh
@@ -5,7 +5,9 @@ sudo systemctl disable NetworkManager
 sudo systemctl stop NetworkManager
 sudo systemctl enable network
 sudo systemctl start network
- 
+
+sudo ip route replace default via 192.168.33.1
+
 sudo yum install -y https://rdoproject.org/repos/rdo-release.rpm
 sudo yum install -y centos-release-openstack-mitaka
 sudo yum update -y
