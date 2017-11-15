7076591850a79a1794b9fdeb225c1cf7abcb5258
Ryo TAKAISHI <ryo.takaishi.0@gmail.com>
2017-11-15 23:57:08 +0900

    不要なので消す

--- provision.sh
+++ provision.sh
@@ -13,11 +13,3 @@ sudo yum install -y centos-release-openstack-mitaka
 sudo yum update -y
 sudo yum install -y openstack-packstack
 
-# Install Golang with gvm
-sudo yum install -y git vim bison
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-source /home/vagrant/.gvm/scripts/gvm
-echo 'source /home/vagrant/.gvm/scripts/gvm' >> ~/.bashrc
-gvm install go1.7.4 -B
-gvm use go1.7.4 --default
-export GOPATH=$HOME
